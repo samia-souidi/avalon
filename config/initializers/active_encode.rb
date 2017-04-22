5c38306c6cfaeec537ccdbfd6dce1a955800f05c
Michael Klein <mbklein@gmail.com>
2017-04-22 00:23:43 -0500

    Direct upload to S3

--- config/initializers/active_encode.rb
+++ config/initializers/active_encode.rb
@@ -7,6 +7,7 @@ when :matterhorn
 when :elastic_transcoder
   MasterFile.default_encoder_class = ElasticTranscoderJob
   pipeline = Aws::ElasticTranscoder::Client.new.read_pipeline(id: Settings.encoding.pipeline)
-  Settings.encoding.masterfile_bucket = pipeline.pipeline.input_bucket
-  Settings.encoding.derivative_bucket = pipeline.pipeline.output_bucket
+  # Set environment variables to guard against reloads
+  ENV['SETTINGS__ENCODING__MASTERFILE_BUCKET'] = Settings.encoding.masterfile_bucket = pipeline.pipeline.input_bucket
+  ENV['SETTINGS__ENCODING__DERIVATIVE_BUCKET'] = Settings.encoding.derivative_bucket = pipeline.pipeline.output_bucket
 end
